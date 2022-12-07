5231f994152667f4790350309823112bac9dfe92
haidang <haidang@DESKTOP-GOU4GL6.localdomain>
2022-12-07 14:19:49 +0700

    push

--- MyS3.py
+++ MyS3.py
@@ -24,7 +24,7 @@ class MyS3():
         return False
     
     
-    def upload_file(self, upload_file, bucket_name, location, public_access = False):
+    def upload_file(self, upload_file, bucket_name, location, public_access):
         '''
         upload_file (File): path dẫn tới file cần upload
         bucket_name (str): tên bucket
@@ -44,39 +44,25 @@ class MyS3():
         if public_access:
             extra_args['ACL'] = 'public-read'
             
-        # try:
-        #     self.write_file(upload_file)
-        #     self.__s3.meta.client.upload_file(
-        #         upload_file.filename, 
-        #         bucket_name, 
-        #         location+upload_file.filename,
-        #         ExtraArgs=extra_args
-        #     )
-        #     return {
-        #         'host': 'https://haidawng-bucket-1.s3.ap-northeast-1.amazonaws.com/',
-        #         'filename': location+upload_file.filename,
-        #         'path': f'https://haidawng-bucket-1.s3.ap-northeast-1.amazonaws.com/{location+upload_file.filename}',
-        #     }
-        # except Exception:
-        #     print('Somewhere went wrong :D')
-        #     return 'Somewhere went wrong :D'
-        # finally:
-        #     print('Xóa file')
-        #     self.delete_file(upload_file.filename)
-        
-        self.write_file(upload_file)
-        self.__s3.meta.client.upload_file(
-            upload_file.filename, 
-            bucket_name, 
-            location+upload_file.filename,
-            ExtraArgs=extra_args
-        )
-        self.delete_file(upload_file.filename)
-        return {
-            'host': 'https://haidawng-bucket-1.s3.ap-northeast-1.amazonaws.com/',
-            'filename': location+upload_file.filename,
-            'path': f'https://haidawng-bucket-1.s3.ap-northeast-1.amazonaws.com/{location+upload_file.filename}',
-        }
+        try:
+            self.write_file(upload_file)
+            self.__s3.meta.client.upload_file(
+                upload_file.filename, 
+                bucket_name, 
+                location+upload_file.filename,
+                ExtraArgs=extra_args
+            )
+            return {
+                'host': 'https://haidawng-bucket-1.s3.ap-northeast-1.amazonaws.com/',
+                'filename': location+upload_file.filename,
+                'path': f'https://haidawng-bucket-1.s3.ap-northeast-1.amazonaws.com/{location+upload_file.filename}',
+            }
+        except Exception:
+            print('Somewhere went wrong :D')
+            return 'Somewhere went wrong :D'
+        finally:
+            print('Xóa file')
+            self.delete_file(upload_file.filename)
         
     def clear_bucket(self, bucket_name):
         bucket = self.get_bucket(bucket_name)
--- router.py
+++ router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, File, UploadFile
 from MyS3 import MyS3
 from typing import List
 from schemas import *
+from decouple import config
 
 router = APIRouter(
     prefix="",
@@ -16,33 +17,33 @@ async def root():
 
 @router.post('/upload_single', summary='upload không public')
 # async def upload(file: UploadFile = File(...), file_location: str = ''):
-async def upload(file: UploadFile = File(...), file_location: str = ''):
+async def upload(file: UploadFile = File(...), dir: str = ''):
     """
     +file (File): file cần upload\n
-    +file_location (str): dir sẽ chứa file, default ở root của bucket\n
+    +dir (str): dir sẽ chứa file, default ở root của bucket\n
     """
     s3 = MyS3()
     result = s3.upload_file(
         upload_file= file, 
-        bucket_name= 'haidawng-bucket-1', 
-        location= file_location,
-        public_access=False
+        bucket_name= config('BUCKET_NAME'), 
+        location= dir,
+        public_access=True
     )
     return result
 
 @router.post('/upload_multi', summary='upload multiple files (chung dir)')
-async def upload_multi(files: List[UploadFile] = File(...), file_location: str = ''):
+async def upload_multi(files: List[UploadFile] = File(...), dir: str = ''):
     """
     +file (File): file cần upload\n
-    +file_location (str): dir sẽ chứa file, default ở root của bucket\n
+    +dir (str): dir sẽ chứa file, default ở root của bucket\n
     """
     for file in files:
         s3 = MyS3()
         result = s3.upload_file(
             upload_file= file, 
-            bucket_name= 'haidawng-bucket-1', 
-            location= file_location,
-            public_access=False
+            bucket_name= config('BUCKET_NAME'), 
+            location= dir,
+            public_access=True
         )
     return result
     
@@ -53,28 +54,30 @@ async def delete(bucket_name: str):
     return result
 
 @router.delete('/delete_file', summary='Xóa file theo tên')
-async def delete(bucket_name: str, file_name: str, file_location: str = ''):
+async def delete( file_name: str, bucket_name: str = None, dir: str = ''):
     """
-    +bucket_name (str): bucket chứa file cần xóa\n
+    +bucket_name (str): bucket chứa file cần xóa (HIỆN TẠI KHÔNG SD, KHÔNG CẦN TRUYỀN)\n
     +file_name (str): tên file cần lấy\n
-    +file_location (str): dir chứa file trong bucket, default ở root của bucket\n
+    +dir (str): dir chứa file trong bucket, default ở root của bucket\n
     """
     s3 = MyS3()
-    result = s3.remove_file(bucket_name, file_name, file_location)
+    bucket_name = config('BUCKET_NAME')
+    result = s3.remove_file(bucket_name, file_name, dir)
     return  result
 
 @router.get('/get_presigned_url', summary='Lấy presigned url')
-async def get_presigned(bucket_name: str, file_name: str, file_location: str = '', expires_time: int = 60):
+async def get_presigned(file_name: str, bucket_name: str = None, dir: str = '', expires_time: int = 60):
     """
-    +bucket_name (str): bucket chứa file cần lấy\n
+    +bucket_name (str): bucket chứa file cần lấy (HIỆN TẠI KHÔNG SD, KHÔNG CẦN TRUYỀN)\n
     +file_name (str): tên file cần lấy\n
-    +file_location (str): dir chứa file trong bucket, default ở root của bucket\n
+    +dir (str): dir chứa file trong bucket, default ở root của bucket\n
     +expires_time (int): số second url có hiệu lực\n
     """
     if file_location[-1] != '/':
         file_location += '/'
     s3 = MyS3()
-    result = s3.get_presigned_url(bucket=bucket_name, key=file_location+file_name, expires_time=expires_time)
+    bucket_name = config('BUCKET_NAME')
+    result = s3.get_presigned_url(bucket=bucket_name, key=dir+file_name, expires_time=expires_time)
     return result
 
 @router.get('/download_file', summary='Download file từ bucket')
